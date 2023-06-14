a2df7486b359eda902c3ef5afac166b32e2386f8
faranora21 <farahuorm21@gmail.com>
2023-06-14 22:51:19 +0800

    added Github repo URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
     Weather app
+
+    <footer>
+      This project is created by Farah Johari and is 
+     <a href="https://github.com/faranora21/react-final-weather" target="_blank" rel="noreferrer">  open-sourced on Github</a>
+    </footer>
     </div>
   );
 }
