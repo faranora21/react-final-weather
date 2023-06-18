ead1a09839aaa1c3caa7801ebf3d11aedcf8b72e
faranora21 <farahuorm21@gmail.com>
2023-06-18 12:31:08 +0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
     Weather app
 
     <footer>
-      This project is created by Farah Johari and is 
-     <a href="https://github.com/faranora21/react-final-weather" target="_blank" rel="noreferrer">  open-sourced on Github</a>
+      This project is created by Farah Johari and is {" "}
+     <a href="https://github.com/faranora21/react-final-weather" target="_blank" rel="noreferrer">open-sourced on Github</a>
     </footer>
+    </div>
     </div>
   );
 }
