3e27ed68b45f3e5d38c4d4ce6ab7d8d0be310e42
faranora21 <farahuorm21@gmail.com>
2023-06-19 23:54:37 +0800

    add styling

--- src/Weather.js
+++ src/Weather.js
@@ -5,31 +5,31 @@ export default function Weather() {
     return (
         <div className="Weather">
         <div className="row justify-content-center">
-            <div className="col-md-6">
+            <div className="col-md-4">
                 <form id="search-form">
+                <div className="input-group mb-3">
                 <input
                   type="text"
                   className="form-control"
                   id="enter-city-input"
                   placeholder="Enter your city..."
                   />
-                   <input type="submit" id="submit-button" value="Search" />
+                   <input className="input-group-text" type="submit" id="submit-button" value="Search" />
+                   </div>
                 </form>
             </div>
         </div>
-        <div className="weather-time">
+        <div className="weather-time text-center">
         <h1>Johor Bahru</h1>
         <ul>
             <li>30 °C | °F</li>
             <li>Sunday 12:48</li>
         </ul>
         </div>
-        <div className="col">
-            <ul>
-                <li>Mostly Cloudy</li>
-                <li>Humidity: 17%</li>
-                <li>Wind: 20km/h</li>
-            </ul>
+        <div className="container weather-info">
+            <div>Mostly Cloudy</div>
+            <div>Humidity: 17%</div>
+            <div>Wind: 20km/h</div>
         </div>
         </div>
     )
